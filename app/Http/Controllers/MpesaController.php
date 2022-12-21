f6e98d0dc6911cbfa0084f3050417daabac0ea1d
Terry-Diana <terry.wdiana@gmail.com>
2022-12-21 11:20:46 +0300

    making changes on MpesaContoller

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -10,17 +10,18 @@ class MpesaController extends Controller
     public function stkSimulator()
     {
         $mpesa = new \Safaricom\Mpesa\Mpesa();
-        $BusinessShortCode = 174379;
-        $LipaNaMpesaPasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-        $TransactionType = "CustomerPayBillOnline";
         $Amount = "1";
-        $PartyA = 254701794453;
         $PartyB = 174379;
-        $PhoneNumber = 254701794453;
-        $CallBackURL = "https://testmpesa.com";
+        $PartyA = "[your mpesa phone number]";
+        $PhoneNumber = "[your mpesa phone number]";
+        $BusinessShortCode = 174379;
         $AccountReference = "Test Payment";
-        $TransactionDesc = "Lipa na Mpesa testing";
         $Remarks = "Test payment successful!";
+        $CallBackURL = "[any URL(real/fake)]";
+        $TransactionType = "CustomerPayBillOnline";
+        $TransactionDesc = "Lipa na Mpesa testing";
+        $LipaNaMpesaPasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+
 
         $stkPushSimulation = $mpesa->STKPushSimulation(
             $BusinessShortCode,
